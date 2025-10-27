46009fa1c451d47c4479ef03310d70ec346b348e
antlerjely96 <antlerjely96@gmail.com>
2025-10-27 09:59:30 +0700

    demo Condition 2

---
+++ Buoi1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+* 2. Nhap so nguyen a tu ban phim. Kiem tra a la so chan hay le 
+*/
+//Khai bao
+int a;
+//Nhap
+Console.Write("a = ");
+a = int.Parse(Console.ReadLine());
+//kiem tra
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " la so chan");
+}
+else
+{
+    Console.WriteLine(a + " la so le");
+}

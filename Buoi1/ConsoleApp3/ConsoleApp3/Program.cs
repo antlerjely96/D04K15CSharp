d3c895f1923c7e94978295890379f6a87978f98a
antlerjely96 <antlerjely96@gmail.com>
2025-10-27 11:02:20 +0700

    demo Condition 2

---
+++ Buoi1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+	3. Nhap so tuoi cua 1 nguoi tu ban phim (tuoi la so nguyen)
+	Kiem tra:
+	Neu tuoi > 0 && tuoi < 6: hoc mau giao
+	Neu tuoi >= 6 && tuoi < 11: hoc cap 1
+	Neu tuoi >= 11 && tuoi < 16: hoc cap 2
+	Neu tuoi >= 16 && tuoi < 18: hoc cap 3
+	Neu tuoi >= 18 && tuoi < 23: Hoc dai hoc
+	Neu tuoi >= 23 && tuoi <= 65: Di lam
+	Con lai: Nghi huu
+	4. Nhap diem trung binh cua 1 hoc sinh.
+	Neu diem > 0 && diem < 5 => Yeu
+	Neu diem >= 5 && diem < 6.5 => trung binh
+	Neu diem >= 6.5 && diem < 8 => Kha
+	Con lai => gioi
+	5. Nhap thang la 1 so nguyen. Hien thi so ngay trong thang do
+	6. Nhap 3 so nguyen a, b, c.
+	Hien thi so lon nhat va so nho nhat trong 3 so
+*/
+

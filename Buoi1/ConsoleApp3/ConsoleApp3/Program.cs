0b71d21cb1b18655c485684b680f9af99cff6570
antlerjely96 <antlerjely96@gmail.com>
2025-11-03 09:14:03 +0700

    demo Loop

--- Buoi1/ConsoleApp3/ConsoleApp3/Program.cs
+++ Buoi1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -3,17 +3,19 @@
 /*
 	3. Nhap so tuoi cua 1 nguoi tu ban phim (tuoi la so nguyen)
 	Kiem tra:
-	Neu tuoi > 0 && tuoi < 6: hoc mau giao
-	Neu tuoi >= 6 && tuoi < 11: hoc cap 1
-	Neu tuoi >= 11 && tuoi < 16: hoc cap 2
-	Neu tuoi >= 16 && tuoi < 18: hoc cap 3
-	Neu tuoi >= 18 && tuoi < 23: Hoc dai hoc
-	Neu tuoi >= 23 && tuoi <= 65: Di lam
+	Neu tuoi < 0: nhap sai
+	Neu tuoi < 6: hoc mau giao
+	Neu tuoi < 11: hoc cap 1
+	Neu tuoi < 16: hoc cap 2
+	Neu tuoi < 18: hoc cap 3
+	Neu tuoi < 23: Hoc dai hoc
+	Neu tuoi <= 65: Di lam
 	Con lai: Nghi huu
 	4. Nhap diem trung binh cua 1 hoc sinh.
-	Neu diem > 0 && diem < 5 => Yeu
-	Neu diem >= 5 && diem < 6.5 => trung binh
-	Neu diem >= 6.5 && diem < 8 => Kha
+	Neu diem < 0 || diem > 10: Nhap sai
+	Neu diem < 5 => Yeu
+	Neu diem < 6.5 => trung binh
+	Neu diem < 8 => Kha
 	Con lai => gioi
 	5. Nhap thang la 1 so nguyen. Hien thi so ngay trong thang do
 	6. Nhap 3 so nguyen a, b, c.
---
+++ Buoi2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+/*
+    Loop: dùng để thực hiện 1 khối lệnh nhiều lần
+    for(tham số 1; tham số 2; tham số 3){
+        khối lệnh;
+    }
+    
+    tham số 1;
+    while(tham số 2){
+        Khối lệnh;
+        tham số 3;
+    }
+    
+    tham số 1;
+    do {
+        khối lệnh;
+        tham số 3;
+    } while (tham số 2);
+    
+    do {
+        tham số 1;
+        khối lệnh;
+    } while (tham số 2);
+*/
+
+/*
+    1. Nhap so nguyen n. Hien thi cac so tu 1 den n
+*/
+
+int n;
+Console.Write("n = ");
+n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(n);
+}
+
+/*
+    2. Nhap so nguyen n tu ban phim.
+	Tinh n!
+	3. Nhap so nguyen n tu ban phim.
+	Tinh tong cac so chia het cho 3 hoac 7 trong khoang tu 1 den n
+	4. Nhap so nguyen n tu ban phim
+	Kiem tra n co phai so nguyen to hay khong
+	5. Nhap so nguyen n tu ban phim
+	Hien thi cac so nguyen to trong khoang tu 1 den n
+	6. Ve hinh
+	    1
+	   121
+	  12321
+	 1234321
+	123454321
+*/
